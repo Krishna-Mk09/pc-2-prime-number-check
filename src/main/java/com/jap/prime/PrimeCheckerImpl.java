51922342d8124c64d01b6bdd7dc218fb2c701e9b
M V K <krishna.mugala9@gmail.com>
2022-09-12 14:15:13 +0530

    added the logic to the class PrimeCheckerImpl

--- src/main/java/com/jap/prime/PrimeCheckerImpl.java
+++ src/main/java/com/jap/prime/PrimeCheckerImpl.java
@@ -1,9 +1,25 @@
 package com.jap.prime;
 
 public class PrimeCheckerImpl {
+
     public boolean primeNumberCheck(int number) {
         // write the logic here to check is a number is prime or not
-        return false;
+        PrimeChecker primeChecker = number1 -> {
+            int count = 0;
+            if (number == 0) {
+                return false;
+            } else {
+                for (int i = 1; i <= number; i++) {
+                    if (number % i == 0) {
+                        count++;
+                    }
+                }
+                return count == 2;
+            }
+        };
+        return primeChecker.isPrimeNumber(number);
     }
-
 }
+
+
+
